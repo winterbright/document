19966a035e94bdaa2f6ca46125b62c1480db3cda
winterbright <winterbright@163.com>
2012-07-03 17:22:20 +0800

    freemarker--velocity.java

---
+++ log/freemarker--velocity.java
@@ -0,0 +1,48 @@
+//freemarker的模版文件以.ftl结尾,velocity的模版文件以.vm结尾
+
+//freemarker
+//1、定义配置文件
+Configuration cfg = new Configuration();  
+//2、加载模版路径
+cfg.setDirectoryForTemplateLoading(new File("E:/ma"));  
+//指定模版如何查看数据模型**
+cfg.setObjectWrapper(new DefaultObjectWrapper());  
+//缓存
+cfg.setCacheStorage(new freemarker.cache.MruCacheStorage(20, 250));  
+//3、加载模版
+Template temp = cfg.getTemplate("example.ftl");  
+//4、定义输出
+Writer out = new OutputStreamWriter(System.out);  
+//5、合并模版与数据模型（数据模型：context）
+temp.process(context, out);  
+out.flush(); 
+
+//1、初始化
+Velocity.init();  
+//2、加载数据模型
+VelocityContext context = new  VelocityContext(root);  
+//3、加载模版 
+Template template = Velocity.getTemplate("html/example.vm");  
+//4、定义输出
+StringWriter sw = new  StringWriter();  
+//5、合并模版与数据模型
+template.merge(context, sw);  
+System.out.print(sw.toString()); 
+
+string： ${name}--${number}--${user.age}
+
+condition(freemarker)：	
+<#if  user ==  "Big Joe" >  
+list iterator-----------  
+<#list list as aa>  
+${aa}  
+</#list>   
+</#if > 
+
+condition(velocity)：	
+#if  ($user ==  "Big Joe" )  
+list iterator-----------  
+#foreach( $aa in $list )  
+$aa  
+#end  
+#end
